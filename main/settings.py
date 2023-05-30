e6f731d13ba98ac6a7d5ab1f4cef9ce266a0443a
unknown <bolot.muxa@gmail.com>
2023-05-30 17:00:01 +0600

    30

--- main/settings.py
+++ main/settings.py
@@ -91,6 +91,13 @@ TEMPLATES = [
 ]
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     "default": {
         "ENGINE": os.environ.get("POSTGRES_ENGINE"),
