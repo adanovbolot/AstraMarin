a1a78d108d344bf5cd78e3d0103d59f3ec13bf00
unknown <bolot.muxa@gmail.com>
2023-06-02 00:54:51 +0600

    2

--- main/settings.py
+++ main/settings.py
@@ -11,8 +11,8 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-# ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ") + ['178.21.8.227', 'voshodback.ru']
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ") + ['178.21.8.227', 'voshodback.ru']
+# ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 AUTH_USER_MODEL = "server.User"
 
